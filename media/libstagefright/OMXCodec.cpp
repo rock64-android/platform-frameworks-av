9a6a951440cbb92673e83ec91be36c6053374707
zyh <zyh@rock-chips.com>
2016-04-08 15:00:36 +0800

    libstagefright:fix audio play rate is not normal
    Change-Id: I23cc578deb9e9c7bb087513187623ad53d92d0bd

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -2507,8 +2507,6 @@ void OMXCodec::onCmdComplete(OMX_COMMANDTYPE cmd, OMX_U32 data) {
             if (mState == RECONFIGURING) {
                 CHECK_EQ(portIndex, (OMX_U32)kPortIndexOutput);
 #if 1
-                if(strstr(mComponentName,"OMX.google.aac.decoder"))
-                {
                 sp<MetaData> oldOutputFormat = mOutputFormat;
                 initOutputFormat(mSource->getFormat());
 
@@ -2518,7 +2516,6 @@ void OMXCodec::onCmdComplete(OMX_COMMANDTYPE cmd, OMX_U32 data) {
                 bool formatChanged = formatHasNotablyChanged(oldOutputFormat, mOutputFormat);
                 if (!mOutputPortSettingsHaveChanged) {
                     mOutputPortSettingsHaveChanged = formatChanged;
-                    }
                 }
 #endif
 
